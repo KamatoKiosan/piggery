b38eff58bff5c9752302646391655359b3de367d
KamatoKiosan <KamatoKiosan@users.noreply.github.com>
2019-05-10 21:26:26 +0200

    Checks percentages sum. Calculates money for piggybanks.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void traverseSubcategories(Category& category, float superpercentage) {
+void distributeMoney(Category& category, const int cents, const float superpercentage = 1.0f) {
     for (Category& subcategory : category.getSubcategories()) {
         cout << endl;
         cout << "Category name: " << subcategory.getName() << endl;
@@ -17,9 +17,31 @@ void traverseSubcategories(Category& category, float superpercentage) {
             cout << "Piggybank name: " << piggybank.getName() << endl;
             cout << "Piggybank percentage: " << piggybank.getPercentage() << endl;
             cout << "Piggybank percentage accumulated: " << superpercentage * subcategory.getPercentage() * piggybank.getPercentage() << endl;
+            cout << "This piggybank gets " << cents * superpercentage * subcategory.getPercentage() * piggybank.getPercentage() / 100  << " credits" << endl;
         }
-        traverseSubcategories(subcategory, superpercentage * subcategory.getPercentage());
+        distributeMoney(subcategory, cents, superpercentage * subcategory.getPercentage());
     }
+}
+
+float calculatePercentageSum(Category& category, const float superpercentage = 1.0f) {
+    float piggybanksPercentageSum = 0.0f;
+    float subPercentage = 0.0f;
+    for (Category& subcategory : category.getSubcategories()) {
+        cout << endl;
+        cout << "Category name: " << subcategory.getName() << endl;
+        cout << "Category percentage: " << subcategory.getPercentage() << endl;
+        cout << "Category percentage accumulated: " << superpercentage * subcategory.getPercentage() << endl;
+
+        for (Piggybank& piggybank : subcategory.getPiggybanks()) {
+            cout << "Piggybank name: " << piggybank.getName() << endl;
+            cout << "Piggybank percentage: " << piggybank.getPercentage() << endl;
+            cout << "Piggybank percentage accumulated: " << superpercentage * subcategory.getPercentage() * piggybank.getPercentage() << endl;
+            piggybanksPercentageSum += superpercentage * subcategory.getPercentage() * piggybank.getPercentage();
+        }
+        subPercentage += calculatePercentageSum(subcategory, superpercentage * subcategory.getPercentage());
+    }
+    float percentage = piggybanksPercentageSum + subPercentage;
+    return percentage;
 }
 
 int main() 
@@ -29,16 +51,26 @@ int main()
     //piggery.testJson();
 
     Category treeRootNode = piggery.getTreeRootNode();
+
     Category category1{"Category1"};
     category1.setPercentage(0.5f);
     Category category11{"Category11"};
     category11.setPercentage(0.5f);
-    Piggybank piggybank11{"Piggybank11"};
-    piggybank11.setPercentage(1.0f);
-    category11.addPiggybank(piggybank11);
+    Piggybank piggybank111{"Piggybank111"};
+    piggybank111.setPercentage(0.5f);
+    category11.addPiggybank(piggybank111);
+    Piggybank piggybank112{"Piggybank112"};
+    piggybank112.setPercentage(0.5f);
+    category11.addPiggybank(piggybank112);
     category1.addSubcategory(category11);
     Category category12{"Category12"};
     category12.setPercentage(0.5f);
+    Piggybank piggybank121{"Piggybank121"};
+    piggybank121.setPercentage(0.5f);
+    category12.addPiggybank(piggybank121);
+    Piggybank piggybank122{"Piggybank122"};
+    piggybank122.setPercentage(0.5f);
+    category12.addPiggybank(piggybank122);
     category1.addSubcategory(category12);
     treeRootNode.addSubcategory(category1);
 
@@ -46,13 +78,28 @@ int main()
     category2.setPercentage(0.5f);
     Category category21{"Category21"};
     category21.setPercentage(0.5f);
+    Piggybank piggybank211{"Piggybank211"};
+    piggybank211.setPercentage(0.5f);
+    category21.addPiggybank(piggybank211);
+    Piggybank piggybank212{"Piggybank212"};
+    piggybank212.setPercentage(0.5f);
+    category21.addPiggybank(piggybank212);
     category2.addSubcategory(category21);
     Category category22{"Category22"};
     category22.setPercentage(0.5f);
+    Piggybank piggybank221{"Piggybank221"};
+    piggybank221.setPercentage(0.5f);
+    category22.addPiggybank(piggybank221);
+    Piggybank piggybank222{"Piggybank222"};
+    piggybank222.setPercentage(0.5f);
+    category22.addPiggybank(piggybank222);
     category2.addSubcategory(category22);
     treeRootNode.addSubcategory(category2);
 
-    traverseSubcategories(treeRootNode, 1.0f);
+
+    float percentageSum = calculatePercentageSum(treeRootNode);
+    cout << "percentageSum: " << percentageSum << endl << endl;
+    distributeMoney(treeRootNode, 100*100);
 
     return 0;
 }
