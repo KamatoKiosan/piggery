0142fab00eb9c327c4e526a687ca468fc8e82284
KamatoKiosan <KamatoKiosan@users.noreply.github.com>
2019-05-20 22:48:25 +0200

    Fill demo table with some demo data.

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,3 +1,3 @@
-CREATE TABLE IF NOT EXISTS category(name TEXT, percentage INTEGER NOT NULL, INTEGER parentId NOT NULL);
+CREATE TABLE IF NOT EXISTS category(name TEXT, percentage INTEGER NOT NULL, parentCategoryId INTEGER);
 CREATE TABLE IF NOT EXISTS piggybank(name TEXT, percentage INTEGER NOT NULL, balanceInCents INTEGER NOT NULL, goalInCents INTEGER NOT NULL, remark TEXT, categoryId INTEGER);
 CREATE INDEX IF NOT EXISTS idx_piggybank_categoryId ON piggybank (categoryId);
